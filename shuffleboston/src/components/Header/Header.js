eebab4c6c218e1873f4d2f89a1dc887440dbf214
Alicia Good <agood@opentext.com>
2018-01-05 12:24:56 -0600

    Copying Shuffleboard source code to repository

---
+++ shuffleboston/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>ShuffleBoston!</h1>
+    <h2>
+      Click on a Boston Terrier image to earn points.  Don't click the Boston twice or you lose the game!
+    </h2>
+  </header>
+);
+
+export default Header;
